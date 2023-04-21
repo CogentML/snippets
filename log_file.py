72c177d899338e6a5cc6e4faf931c07458bd7b84
CogentML <123573328+CogentML@users.noreply.github.com>
2023-04-21 10:22:31 -0400

    Initial Commit

---
+++ log_file.py
@@ -0,0 +1,25 @@
+import logging
+
+# Create a logger and set its level to DEBUG
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# Create a file handler that logs all messages to a file named 'output.log'
+file_handler = logging.FileHandler('output1.log')
+file_handler.setLevel(logging.DEBUG)
+
+# Create a formatter that formats log messages
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+
+# Add the file handler to the logger
+logger.addHandler(file_handler)
+
+def square_n_numbers(n):
+    for i in range(1, n+1):
+        square = i * i
+        print(f"{i} squared is {square}")
+        logger.debug(f"{i} squared is {square}")
+
+# Test the function
+square_n_numbers(5)
